a689d67a27921cda6f98987d5a9c17f8af95963d
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-10-26 08:50:41 -0400

    Add files via upload

---
+++ 001 - Using R as a Calculator -a first session using.R
@@ -0,0 +1,40 @@
+# Shortcut to run commands> ctrl+enter 
+
+# Basic Operations - Using R as a Calculator
+# Comments in R are done using the # - 
+# this line and the above will not be compiled as code
+
+# Summing values
+1 + 1 
+
+# Subtraction
+10 - 5
+
+# Subtracting floats
+10-5.3
+
+# Division
+10 / 2 
+
+# Multiplication
+2 * 3
+
+# Square Roots
+sqrt(9)
+
+# Exponentials
+exp(1)
+
+# Logarithms
+log(2)
+
+# Powers
+4**2
+
+# More Complex Calculations using 
+# Mathematical Order of Calculations
+(10+10)/(2**2)
+
+(log(3)/100)/sqrt(9)
+
+
