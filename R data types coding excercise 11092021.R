2c4169831767b59a9c2bf53f46772796e2f3dd9c
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-11-09 11:10:00 -0500

    Add files via upload

---
+++ R data types coding excercise 11092021.R
@@ -0,0 +1,18 @@
+# Coding Exercise ITS 150
+# the bits of code that you need to complete are the 
+# variables in the code assigned to 0 
+
+# This is our example vector (don't change this)
+years &lt;- c(2008,2009,2010,2011,2012,2013, "YEAR")
+
+# Extract the underlying data type of the vector years
+# and store it in a variable called type_years
+type_years = 0
+
+# Extract the class data type of the vector years
+# and store it in a variable called class_type_years
+class_type_years = 0
+
+# Test if the vector years is of a numeric class type 
+# and store the result in a variable is_numeric
+is_numeric  = 0s
